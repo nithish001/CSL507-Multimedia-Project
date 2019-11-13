ea00ebe18eaea2651909775b5642737cef49d4ce
nithish001 <sunnynithish@gmail.com>
2019-11-13 13:10:37 +0530

    Voice Parts Extraction Completed

--- AudioFeatureExtract.py
+++ AudioFeatureExtract.py
@@ -7,29 +7,19 @@ def getSamplingRateFromFile(filename):
     song = AudioSegment.from_file(filename)
     return song.frame_rate
 
-def getTotalSamples(filename,sr):
-    y, sr = librosa.core.load(filename,sr=sr)
-    return len(y)
-
 
 def calculateTempoFromFile(filename, hop_length=512):
     sr = getSamplingRateFromFile(filename)
     y, sr = librosa.core.load(filename,sr)
-    onset_env = librosa.onset.onset_strength(y, sr=sr)
-    print('onset samples ', len(onset_env))
     tempo = librosa.beat.tempo(y=y, sr=sr, aggregate=None, hop_length=hop_length)
-    print('total samples ',len(y))
-    print('tempo samples ',len(tempo))
     return tempo
 
-def calculateDerivativeTempo(tempo):
-    dtempo = [tempo[i+1] - tempo[i] for i in range(len(tempo)-1)]
-    return np.array(dtempo)
+# def calculateDerivativeTempo(tempo):
+#     dtempo = [tempo[i+1] - tempo[i] for i in range(len(tempo)-1)]
+#     return np.array(dtempo)
 
 ##Reference : https://librosa.github.io/librosa_gallery/auto_examples/plot_vocal_separation.html
-def getVocalPartsFromFile(filename,sr):
-    y, sr = librosa.core.load(filename, duration=None,sr=sr)
-    print('sampling rate is ',sr)
+def getVocalPartsFromSignal(y,sr):
     # And compute the spectrogram magnitude and phase
     S_full, phase = librosa.magphase(librosa.stft(y))
     S_filter = librosa.decompose.nn_filter(S_full,
@@ -47,46 +37,63 @@ def getVocalPartsFromFile(filename,sr):
     S_foreground = mask_v * S_full
     D_foreground = S_foreground * phase
     y_foreground = librosa.istft(D_foreground)
-    librosa.output.write_wav('foreground.wav', y_foreground, sr)
     return y_foreground
 
-def plotTempoTimeGraph(tempo, hop_length, sr, totSamples):
+def plotFeatureTimeGraph(feature, hop_length, sr):
 
-    x = [i*hop_length/sr for i in range(len(tempo))]
-    y = list(tempo)
+    x = [i*hop_length/sr for i in range(len(feature))]
+    y = list(feature)
     plt.plot(x,y)
     plt.show()
 
-def plotAudioTimeGraph(y, sr):
-    y1 = []
-    for i in y:
-        if(abs(i) > 0.65):
-            y1.append(i)
-        else:
-            y1.append(0)
-    plt.plot([i/float(sr) for i in range(len(y1))], y1)
-    plt.show()
-    plt.plot([i/float(sr) for i in range(len(y))], y)
-    plt.show()
+def getNoiseRemovedVoiceParts(voice_parts, thresh,sr):
+    frame_length = 512
+    hop_length = 512
+    rms = librosa.feature.rms(voice_parts, frame_length=frame_length, hop_length=hop_length)
+    rms = rms.T
+    for i in range(rms.shape[0]):
+        if(rms[i] < thresh):
+            end = min((i+1)*hop_length, voice_parts.shape[0])
+            voice_parts[i*hop_length : end] = 0.0
+    
+    #librosa.output.write_wav('noise_removed.wav', voice_parts, sr)
+    return voice_parts
 
+def extractVoiceParts(filename,thresh):
+    sr = getSamplingRateFromFile(filename)
+    y,sr = librosa.core.load(filename,sr)
+    voice_parts = getVocalPartsFromSignal(y,sr)
+    fin_voice_parts = getNoiseRemovedVoiceParts(voice_parts, thresh, sr)
+    fin_voice_parts[fin_voice_parts > 0] = 1
+     
 
+    
 
 
 
 
 
 if __name__ == '__main__':
-    hop_length = 1024
-    filename = 'see_you_again.mp4'
+    hop_length = 512
+    filename = 'temp/foreground.wav'
     sr = getSamplingRateFromFile(filename)
     # print('sampling rate is ',sr)
     # totSamples = getTotalSamples(filename,sr)
     # tempo = calculateTempoFromFile(filename,hop_length)
-    # plotTempoTimeGraph(tempo, hop_length, sr,totSamples)
-    voice_mag = getVocalPartsFromFile(filename,sr)
-    #print('total samples ',totSamples)
-    print('voice samples ',len(voice_mag))
-    plotAudioTimeGraph(voice_mag, sr)
+    # plotFeatureTimeGraph(tempo, hop_length, sr,totSamples)
+    # voice_mag = getVocalPartsFromFile(filename,sr)
+    # #print('total samples ',totSamples)
+    # print('voice samples ',len(voice_mag))
+    # plotAudioTimeGraph(voice_mag, sr)
+    # voice_parts = getVocalPartsFromFile(filename,sr)
+    voice_parts,sr = librosa.core.load(filename,sr)
+    print(voice_parts.shape)
+    rmse = librosa.feature.rms(voice_parts, frame_length=512, hop_length=512)
+    print(rmse.shape)
+    rmse = rmse.T
+    plotFeatureTimeGraph(rmse, 512, sr)
+    getNoiseRemovedVoiceParts(voice_parts, 0.025, sr)
+
 
 
 
--- face_emotion_detector.py
+++ face_emotion_detector.py
@@ -28,6 +28,7 @@ def detect_face(frame):
 def face_emotion_analysis(video_filename):
     video_capture = cv2.VideoCapture(video_filename)
     emotion_detector = FER()
+    fps = video_capture.get(cv2.CAP_PROP_FPS)
     frame_number = 1
     frame_emotion_dict = {}
     while True:
@@ -40,7 +41,7 @@ def face_emotion_analysis(video_filename):
         current_emotion = None
         if(face_detect == 1):
             current_emotion = get_emotion_face(emotion_detector, rgb_frame)
-        frame_emotion_dict[frame_number] = current_emotion
+        frame_emotion_dict[frame_number/float(fps)] = current_emotion
         frame_number+=1
     save_file_name = os.path.basename(video_filename)
     save_file_name += 'face_emotion.json'
@@ -48,48 +49,6 @@ def face_emotion_analysis(video_filename):
         json.dump(frame_emotion_dict, fp)
 
 
-
-
-# haar_file = 'haarcascade_frontalface_default.xml'
-# # Get a reference to webcam 
-# video_capture = cv2.VideoCapture("/home/sunny/semester 7/multimedia/Project/Dataset/sunflower.mp4")
-# face_cascade = cv2.CascadeClassifier(haar_file) 
-# # Initialize variables
-# face_locations = []
-# detector = FER()
-# while True:
-#     # Grab a single frame of video
-#     ret, frame = video_capture.read()
-
-#     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-#     rgb_frame = frame[:, :, ::-1]
-
-#     # Find all the faces in the current frame of video
-    
-#     face_locations = face_cascade.detectMultiScale(frame, 1.3, 4)
-#     # for (x, y, w, h) in faces: 
-#     #     cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2) 
-#     #face_locations = face_recognition.face_locations(rgb_frame)
-#     # #print(face_locations)
-#     # # # Display the results
-#     # # for top, right, bottom, left in face_locations:
-#     #     # Draw a box around the face
-        
-#     if(len(face_locations) > 0):
-#         for (x, y, w, h) in face_locations:
-#             emotion = get_emotion_face(detector, frame)
-#             print('emotion is ', emotion)
-#             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-#     # Display the resulting image
-            
-#     cv2.imshow('Video', frame)
-#     # Hit 'q' on the keyboard to quit!
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-
-# # Release handle to the webcam
-# video_capture.release()
-# cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     video_filename = '/home/sunny/semester 7/multimedia/Project/Dataset/sunflower.mp4'
